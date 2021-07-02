f662993be633b5716b4fc05403065187566c2c17
Srdjan Markovic <srdjan1408@gmail.com>
2021-07-02 22:12:52 +0200

    Modifikacija

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -18,6 +18,7 @@
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <p>{{ $post->tekst }}</p>
+                <h1>Srdjan</h1>
             </div>
         </div>
     </div>
